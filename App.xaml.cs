7efdbf4d888ec9a597f92ffda3f7db366a505618
Stela H. Seo <stela.h.seo@gmail.com>
2020-08-14 16:53:00 -0500

    Update the license to GNU

--- App.xaml.cs
+++ App.xaml.cs
@@ -23,15 +23,13 @@ namespace FASTASelector
         {
             get
             {
-                return "The MIT License (MIT)"
-                    + Environment.NewLine + Environment.NewLine
-                    + "Copyright (c) Stela H. Seo <stela.seo@cs.umanitoba.ca>"
-                    + Environment.NewLine + Environment.NewLine
-                    + "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"
-                    + Environment.NewLine + Environment.NewLine
-                    + "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
-                    + Environment.NewLine + Environment.NewLine
-                    + "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+                return "Copyright (C) 2020  Stela H. Seo <stela.seo@cs.umanitoba.ca>" +
+                    Environment.NewLine + Environment.NewLine +
+                    "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version." +
+                    Environment.NewLine + Environment.NewLine +
+                    "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details." +
+                    Environment.NewLine + Environment.NewLine +
+                    "You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.";
             }
         }
 
